054c1aae7805f9b194c9decbf13f2de33bc9362e
Demira11 <117664846+Demira11@users.noreply.github.com>
2023-11-15 13:35:03 -0500

    Added a on click submit function so I can work on the second page while waiting for the backend to be fully done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 import Login from "./pages/Login.jsx";
 import Budget from "./pages/Budget.jsx";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Login />
-      <Budget />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/budget" element={<Budget />} />
+      </Routes>
     </div>
   );
 }
