0028f7fce0cc98273b6cc0347d87c8415f84bdc1
Demira11 <117664846+Demira11@users.noreply.github.com>
2023-11-17 12:29:30 -0500

    Created the doughnut chart on ChartPage.jsx

--- src/pages/ChartPage.jsx
+++ src/pages/ChartPage.jsx
@@ -1,14 +1,43 @@
-import{
-  Chart as ChartJS,
-  ArcElement,
-  ToolTip,
-  Legend
+import React from "react";
+import { Doughnut } from "react-chartjs-2";
+
+function ChartPage() {
+  const data = {
+    labels: ["Yes", "No"],
+    datasets: [
+      {
+        label: "Poll",
+        data: [3, 6],
+        backgroundColor: ["green", "red"],
+        borderColor: ["white"],
+      },
+    ],
+  };
+
+  const options = {
+    plugins: {
+      tooltip: {
+        callbacks: {
+          label: function (context) {
+            return context.parsed + " votes";
+          },
+        },
+      },
+    },
+  };
+
+  return (
+    <div className="App">
+      <h1>Budget Status</h1>
+
+      <div style={{ width: "50%", height: "50%" }}>
+        <Doughnut data={data} options={options}></Doughnut>
+      </div>
+    </div>
+  );
 }
 
-
-
-
-
+export default ChartPage;
 
 // import React, { useState } from "react";
 // import { Doughnut } from "react-chartjs-2";
