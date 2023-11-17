64f980f825ef17c1349faa28f11d49da358e8a3e
harry123001 <singhharkirat9090@gmail.com>
2023-11-17 12:40:56 -0500

    Added apiconfig file

---
+++ src/services/apiConfig.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+let apiUrl;
+
+const apiUrls = {
+  production: "https://ddj-budget-hub-app-ac59ef042ed9.herokuapp.com/api",
+  development: "http://localhost:3000/api",
+};
+
+if (window.location.hostname === "localhost") {
+  apiUrl = apiUrls.development;
+} else {
+  apiUrl = apiUrls.production;
+}
+
+const api = axios.create({
+  baseURL: apiUrl,
+});
+
+export default api;
