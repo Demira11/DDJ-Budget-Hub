69e98ad97ff39f3a5f1aab46f9a0065845e51dee
harry123001 <singhharkirat9090@gmail.com>
2023-11-17 15:01:00 -0500

    edit apiConfig

--- src/services/apiConfig.js
+++ src/services/apiConfig.js
@@ -4,7 +4,7 @@ let apiUrl;
 
 const apiUrls = {
   production: "https://ddj-budget-hub-app-ac59ef042ed9.herokuapp.com/api",
-  development: "http://localhost:3000/api",
+  development: "https://ddj-budget-hub-app-ac59ef042ed9.herokuapp.com/api",
 };
 
 if (window.location.hostname === "localhost") {
