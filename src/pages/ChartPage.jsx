1447c1c2551f3cbbea24beb20168fafdeadb7466
Demira11 <117664846+Demira11@users.noreply.github.com>
2023-11-20 10:37:33 -0500

    Changed chart size

--- src/pages/ChartPage.jsx
+++ src/pages/ChartPage.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "../ChartPage.css";
 import { PieChart, Pie, Tooltip } from "recharts";
+import { NavLink } from "react-router-dom"; // Make sure to import NavLink if you are using it
 
 const ChartPage = () => {
   const data = [
-    { name: "Income", value: 3000000000 },
+    { name: "Income", value: 2000000000 },
     { name: "Housing", value: 100000000 },
     { name: "Groceries", value: 100000000 },
     { name: "Utilities", value: 100000000 },
@@ -16,32 +17,45 @@ const ChartPage = () => {
   ];
 
   return (
-    <PieChart width={400} height={400}>
-      <Pie
-        dataKey="value"
-        isAnimationActive={false}
-        data={data}
-        cx="50%"
-        cy="50%"
-        outerRadius={80}
-        fill="#8884d8"
-        label
-      />
+    <div className="chartPage-container">
+      <nav className="navbar">
+        <div className="nav-title-chart">𑁍DDJ Budget Hub</div>
+        <NavLink to="/" className="nav-item">
+          Home
+        </NavLink>
+        <NavLink to="/budget" className="nav-item">
+          Budget
+        </NavLink>
+        <NavLink to="/logout" className="nav-item">
+          Logout
+        </NavLink>
+      </nav>
 
-      <Tooltip />
-    </PieChart>
+      <PieChart width={400} height={400}>
+        <Pie
+          dataKey="value"
+          isAnimationActive={true}
+          data={data}
+          cx="50%"
+          cy="50%"
+          outerRadius={80}
+          fill="#8884d8"
+          label
+        />
+        <Tooltip />
+      </PieChart>
+    </div>
   );
 };
 
 export default ChartPage;
 
-// import React from "react";
-// import { NavLink } from "react-router-dom";
+//  import { NavLink } from "react-router-dom";
 // import { Doughnut } from "react-chartjs-2";
 
-// function ChartPage() {
-//   const data = {
-//     labels: ["Yes", "No"],
+// function ChartPage() { */}
+// {/* //   const data = { */}
+// {/* //     labels: ["Yes", "No"],
 //     datasets: [
 //       {
 //         label: "Poll",
